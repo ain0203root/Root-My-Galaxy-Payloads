7a170b06ed243f4a0d2ee5c4f50f85ed97269c31
ain0203root <ain0203root@gmail.com>
2026-09-09 21:52:23 +0300

    Add S721W experimental P0 fingerprint header

---
+++ src/targets/r12s-S721WVLSCDZF4/p0_fingerprint.h
@@ -0,0 +1,7 @@
+#ifndef R12S_S721WVLSCDZF4_P0_FINGERPRINT_H
+#define R12S_S721WVLSCDZF4_P0_FINGERPRINT_H
+
+/* Placeholder inherited from the verified S721B profile. Replace with W-specific
+ * fingerprint data once it is recovered from S721WVLSCDZF4. */
+
+#endif
